95241572ceb763cf9d83a04458296d34e4fa8e84
sy201901y <70794009+sy201901y@users.noreply.github.com>
2022-02-09 21:34:35 +0800

    Update basic_definations.h

--- basic_definations.h
+++ basic_definations.h
@@ -1,5 +1,4 @@
 #ifdef SYONE_LIB
-#ifdef STD
 #include<algorithm>
 #include<iostream>
 #include<cstring>
@@ -27,9 +26,53 @@
 #define neg_set_inf 0x80808080
 #define For(i,l,r) for(int i=l;i<=r;i++)
 #define roF(i,l,r) for(int i=r;i>=l;i--)
-#include "algo/bfs.h"
+#ifndef ONLINE_JUDGE
 #include "basics/input.h"
-#ifdef BINARY
-#include "DS/bitcompress.h"
+#else
+#ifndef READ
+#define READ
+#include<cstdio>
+#include<cctype>
+namespace syone{
+namespace read
+{
+	template<typename T>T getIntegral()
+	{
+		int X=0; bool flag=1; char ch=getchar();
+		while(ch<'0'||ch>'9') {if(ch=='-') flag=0; ch=getchar();}
+		while(ch>='0'&&ch<='9') {X=(X<<1)+(X<<3)+ch-'0'; ch=getchar();}
+		if(flag) return X;
+		return -X;
+	}
+	char getChar()
+	{
+		char ch;
+		do{ch=getchar();}while(ch<=' ');
+		return ch;
+	}
+}
+namespace readwithfread
+{
+#define getchar()(p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
+	char buf[1<<21],*p1=buf,*p2=buf;
+	template<typename T>T getIntegral()
+	{
+		int X=0; bool flag=1; char ch=getchar();
+		while(ch<'0'||ch>'9') {if(ch=='-') flag=0; ch=getchar();}
+		while(ch>='0'&&ch<='9') {X=(X<<1)+(X<<3)+ch-'0'; ch=getchar();}
+		if(flag) return X;
+		return -X;
+	}
+	char getChar()
+	{
+		char ch;
+		do{ch=getchar();}while(ch<=' ');
+		return ch;
+	}
+#undef getchar()
+}
+}
+#endif
+#endif
 #endif
 #endif
