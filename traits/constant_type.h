1068c963a3c19a75e3581e8626671b5009b6e56e
sy201901y <70794009+sy201901y@users.noreply.github.com>
2022-09-23 17:56:01 +0800

    Update constant_type.h

--- traits/constant_type.h
+++ traits/constant_type.h
@@ -3,9 +3,10 @@
 #define CONSTANT_TYPE
 namespace syone
 {
-	template<typename T,T x>struct Value{using ValueType=T;T value=x;}
-	using Truetype=Value<bool,true>;
-	using Falsetype=Value<bool,false>;
+	template<typename T,T x>struct Integral_constant{using ValueType=T;T value=x;}
+	using True_type=Integral_constant<bool,true>;
+	using False_type=Integral_constant<bool,false>;
+	template<int x>struct Integer_wrapper=Integral_constant<int,x>;
 }
 #endif
 #endif
