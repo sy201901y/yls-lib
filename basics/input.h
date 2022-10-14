2c9d9e67e824947c7ecdc50b3c6281a4a4b0d85b
sy201901y <70794009+sy201901y@users.noreply.github.com>
2022-10-14 19:48:12 +0800

    Update input.h

--- basics/input.h
+++ basics/input.h
@@ -4,43 +4,18 @@
 #include<cstdio>
 #include<cctype>
 namespace syone{
-namespace read
-{
-	template<typename T>T getIntegral()
-	{
-		int X=0; bool flag=1; char ch=getchar();
-		while(ch<'0'||ch>'9') {if(ch=='-') flag=0; ch=getchar();}
-		while(ch>='0'&&ch<='9') {X=(X<<1)+(X<<3)+ch-'0'; ch=getchar();}
-		if(flag) return X;
-		return -X;
-	}
-	char getChar()
-	{
-		char ch;
-		do{ch=getchar();}while(ch<=' ');
-		return ch;
-	}
-}
-namespace readwithfread
-{
-#define getchar()(p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
-	char buf[1<<21],*p1=buf,*p2=buf;
-	template<typename T>T getIntegral()
-	{
-		int X=0; bool flag=1; char ch=getchar();
-		while(ch<'0'||ch>'9') {if(ch=='-') flag=0; ch=getchar();}
-		while(ch>='0'&&ch<='9') {X=(X<<1)+(X<<3)+ch-'0'; ch=getchar();}
-		if(flag) return X;
-		return -X;
-	}
-	char getChar()
-	{
-		char ch;
-		do{ch=getchar();}while(ch<=' ');
-		return ch;
-	}
-#undef getchar()
-}
+	static char buf[1000000],*p1=buf,*p2=buf,obuf[1000000],*p3=obuf;
+	#define getchar() p1==p2&&(p2=(p1=buf)+fread(buf,1,1000000,stdin),p1==p2)?EOF:*p1++
+	#define putchar(x) (p3-obuf<1000000)?(*p3++=x):(fwrite(obuf,p3-obuf,1,stdout),p3=obuf,*p3++=x)
+	#define cin BAD_INPUT_WAY
+	#define cout BAD_OUTPUT_WAY
+	template<typename item=int>inline void read( item ref x){x=0; int f=1; char c=getchar();while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}while(c>='0'&&c<='9')x=(x<<3)+(x<<1)+(c^48),c=getchar();x*=f;}
+	template<typename item=int,typename... items>inline void read(item ref x,items ref ... args){read(x);read(args...);}
+	static char cc[20];
+	template<typename item=int>inline void print_helper( item x){  int len=0;if(x<0)x=-x,putchar('-');while(x)cc[len++]=x%10+'0',x/=10;while(len--)putchar(cc[len]);}
+	template<typename item=int>inline void print( item x){(x)?(print_helper(x),0):(putchar('0'));}
+	template<typename item=int,typename... items>inline void print(item x,items ... args){print(x);print(args...);}
+	struct IO_HELPER{~IO_HELPER(){fwrite(obuf,p3-obuf,1,stdout);}}some_io;
 }
 #endif
 #endif
